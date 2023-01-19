f76cbbfff25aaa47d0c762d0959eaf3e613b8faf
Chase Forestello <chase.forestello@gmail.com>
2023-01-19 17:11:58 -0600

    full search functionality

--- src/App.js
+++ src/App.js
@@ -1,23 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
+import searchIcon from "./searchIcon.svg";
+import MovieCard from "./components/MovieCard";
+
+const API_URL =
+  "https://api.themoviedb.org/3/search/movie?api_key=6ce0041b387bdc16ed1d50bd0aed4f0a&language=en-US&page=1&include_adult=false&query=";
+
+const movie1 = {
+  adult: false,
+  backdrop_path: "/eNI7PtK6DEYgZmHWP9gQNuff8pv.jpg",
+  genre_ids: [878, 28, 12],
+  id: 624860,
+  original_language: "en",
+  original_title: "The Matrix Resurrections",
+  overview:
+    "Plagued by strange memories, Neo's life takes an unexpected turn when he finds himself back inside the Matrix.",
+  popularity: 103.1,
+  poster_path: "/8c4a8kE7PizaGQQnditMmI1xbRp.jpg",
+  release_date: "2021-12-16",
+  title: "The Matrix Resurrections",
+  video: false,
+  vote_average: 6.6,
+  vote_count: 4474,
+};
 
 function App() {
+  const [movies, setMovies] = useState([]);
+  const [search, setSearch] = useState("");
+
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}${title}`);
+    const data = await response.json();
+
+    setMovies(data.results);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Chase's Movie Search</h1>
+      <div className="search">
+        <input
+          type="text"
+          placeholder="Search for a movie"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+        <img
+          src={searchIcon}
+          alt="search"
+          onClick={() => searchMovies(search)}
+        />
+      </div>
+      {movies.length > 0 ? (
+        <div className="container">
+          {movies.map((movie) => (
+            <MovieCard movie={movie} />
+          ))}
+        </div>
+      ) : (
+        <div className="empty">
+          <h2>No movies found</h2>
+        </div>
+      )}
+      {/* <!-- Modal --> */}
+<div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div className="modal-dialog">
+    <div className="modal-content">
+      <div className="modal-header">
+        <h1 className="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+        <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div className="modal-body">
+        ...
+      </div>
+      <div className="modal-footer">
+        <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" className="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
     </div>
   );
 }
---
+++ src/components/MovieCard.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+const MovieCard = ({ movie }) => {
+  return (
+    <div className="movie">
+      <div>
+        <p>{movie.relase_date}</p>
+      </div>
+      <div>
+        <img
+          src={
+            movie.poster_path !== null
+              ? "https://image.tmdb.org/t/p/original" + movie.poster_path
+              : "https://via.placeholder.com/400"
+          }
+          alt="movie poster"
+        />
+      </div>
+      <div>
+        <span>{movie.vote_average}/10</span>
+        <h2>{movie.title}</h2>
+        {/* <!-- Button trigger modal --> */}
+        <button
+          type="button"
+          className="btn btn-dark"
+          data-bs-toggle="modal"
+          data-bs-target="#exampleModal"
+        >
+          Launch demo modal
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default MovieCard;
